b18af1f1913b17aa9a9771370fdf86744dc86bb2
peleg <peleg@home365.co>
2023-01-08 12:49:47 +0200

    real

--- run_sample.py
+++ run_sample.py
@@ -6,23 +6,37 @@ import pickle
 def score(x_f):
     feat_dict = json.loads(x_f)
     feat_dict["credit_score"] = feat_dict["credit_score"]/1000
-    feat_dict["age"] = feat_dict["age"]/100
+    feat_dict["age"] = feat_dict["age"]/85
     mod = pd.DataFrame([feat_dict])
     model = pickle.load(open("tenant_score_ml_model", 'rb'))
     ans = model.predict(mod)
+    print("tenant scoring: ")
     if ans > 1:
-        return 100
+        fans = 100
+    elif ans < 0:
+        fans = 20
     elif 0.85 > ans:
-        return ans*100+7
+        fans = ans*100+7
     else:
-        return ans*100
+        fans = ans*100
+    if fans < 25:
+        level = 1
+    elif fans < 45:
+        level = 2
+    elif fans < 60:
+        level = 3
+    elif fans < 80:
+        level = 4
+    else:
+        level = 5
+    return {'level': level, 'score': fans}
 
 
 
 
-x = """{"credit_score": 486,
-        "eviction": 1,
+x = """{"credit_score": 678,
+        "eviction": 2,
         "criminal": 1,
-        "age": 29}
+        "age": 28}
     """
 score = score(x)
